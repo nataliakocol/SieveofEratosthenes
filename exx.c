ffae60e4f9b44541e9343c07bc8d2328249a3bbd
nataliakocol <43788637+nataliakocol@users.noreply.github.com>
2025-08-15 23:07:43 +0100

    Update exx.c

--- exx.c
+++ exx.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int z;
 int i;
-const int N = 10000000 + 1; //or #define N 100...
+const int N = 10000000 + 1;
 int sieve[N], B[N];
 int gcd (int a, int b) {
 
@@ -14,12 +14,7 @@ int gcd (int a, int b) {
 
 int main() {
     printf("0");
-    //return 0;
     
-
-//}
-    
-//// works for smallest prime gcd
     int j;
     sieve[1] = 1;
     for (i = 2; i*i < N; i++) {
@@ -31,7 +26,7 @@ int main() {
                     sieve[j] = i;
                 }
             }
-           // sieve[i] = i;
+            
         }
 
     }
@@ -49,49 +44,8 @@ int main() {
         int a, b;
         scanf("%d %d", &a, &b);
         printf("%d %d %d\n", B[b] - B[a - 1], sieve[a], sieve[b]);
-        //or for (i = 0...
-
+        
 
 
     }
         
-//        int n, j, prime = 1;
-//        scanf("%d", &n);
-//
-//        for (j = 2; j*j <= n; j++) {
-//
-//            if (n % j == 0) {
-//
-//                printf("%d jest liczba zlozona\n", n);
-//                prime = 0;
-//                break;
-//            }
-//        }
-//
-//        if (prime == 1)
-//            printf("%d jest liczba pierwsza\n", n);
-    
-    
-//    scanf("%d", &z);
-//    while (z--) {
-//
-//        int a, b;
-//        int t;
-//        scanf("%d &d", &a, &b);
-//
-//        printf("%d %d\n", a, b);
-//
-//        while (b) {
-//
-//            t = b;
-//            b = a % b;
-//            a = t;
-//
-//        }
-//        printf("%d\n", a);
-//        printf("%d\n", gcd(a, b));
-//
-//
-//    }
-//    return 0;
-}
